a089d511d5b6e95e6ed6b8563d034de2b5a33993
jenkinsbe <36612511+jenkinsbe@users.noreply.github.com>
2020-08-06 17:36:20 +1000

    Added new plot to figure
    Now display scaled magnitude and actual magnitude in dB based on microphone database.
    
    https://dsp.stackexchange.com/questions/32076/fft-to-spectrum-in-decibel

--- wav2fft2img.py
+++ wav2fft2img.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import peakutils
 import pyaudio
 import wave
 import socket
@@ -6,9 +11,11 @@ import sys
 import os
 import argparse
 from scipy.fftpack import rfft
-import numpy as np
 from scipy.io import wavfile as wav
+import matplotlib.pyplot as plt
 
+
+plt.close('all')
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
@@ -16,6 +23,8 @@ RATE = 44100
 CHUNK = 512
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = socket.gethostname() + "_" + datetime.now().strftime("%Y%m%d_%H%M%S") + ".wav"
+Y_AXIS_MAX = 5000
+
 
 def get_audio_device():
     audio = pyaudio.PyAudio()
@@ -64,33 +73,6 @@ def printAudioDevices():
         if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
             print("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
 
-def perform_FFT(_filename, _upper_freq = 4000):
-
-    Fs, data = wav.read(_filename)
-    N = len(data)           # Number of sample points
-    T = 1.0 / Fs            # sample spacing
-    upper_cutoff = int(_upper_freq / (Fs / N) * 2)
-    channels = len(data.shape)
-
-    print ('Data type          :', data.dtype)
-    if data.dtype == 'int16':
-        data = data / 32767     # 16bit int to -1,1    
-
-    print ('Sampling frequency :', Fs, 'Hz')
-    print ('Sample length      :', len(data), 'seconds')
-    print ('Audio channels     :', channels)
-
-    if channels == 2:
-        data = data.sum(axis=1) / 2
-
-    print ('Audio length       :', '{:0.3f}'.format(N / Fs), 'seconds')
-
-    # perform FFT
-    xf = np.linspace(0.0, 1.0/(2.0*T), N)
-    yf = 2*abs(rfft(data))/N
-
-    return (xf, yf, upper_cutoff)
-
 def cm2inch(*tupl):
     inch = 2.54
     if isinstance(tupl[0], tuple):
@@ -98,22 +80,81 @@ def cm2inch(*tupl):
     else:
         return tuple(i/inch for i in tupl)
         
-def saveToImage(_xf, _yf, _upper_cutoff, _filename, _title="FFT"):
-    # set up graphics screen to plots
-    import matplotlib.pyplot as plt
+def saveToImage(freq, s_mag, s_db, _upper_cutoff, _filename, _title="FFT"):
     
-    plt.figure(num=None, figsize=(16, 6), dpi=100)
+    plt.figure(num=None, figsize=(20, 12), dpi=100)
     plt.title (_title)
-    plt.ylim(0, 0.1)
-    plt.plot(_xf[:_upper_cutoff], _yf[:_upper_cutoff])
-    plt.grid()
-    plt.savefig(_filename)
-#    plt.show()
+    
+    ax1 = plt.subplot(211, xlabel='Frequency [Hz]', ylabel='Amplitude [scaled]', xlim=(0,_upper_cutoff), ylim=(0,1.1))
+    ax1.plot(freq, s_mag, 'r')
+    ax1.grid(True)
+    peaks = peakutils.indexes(s_mag, thres=0.1, min_dist=10)    # find the peaks
+    print ('s_mag peaks:', peaks)
+    for peak in peaks:
+        s = str(round(freq[peak]))[:-2]
+        ax1.annotate(s, [freq[peak], s_mag[peak]], xytext=[freq[peak]-45, s_mag[peak]+0.025])
+        print (freq[peak], s_db[peak])
 
-def sendWavViaFTP(_outputdir, _filename):
+    ax2 = plt.subplot(212, xlabel='Frequency [Hz]', ylabel='Amplitude [dB]', xlim= (0,_upper_cutoff), ylim=(-60,100))
+    ax2.plot(freq, s_db, 'b')
+    ax2.grid(True)
+    peaks = peakutils.indexes(s_db, thres=0, min_dist=50)    # find the peaks
+    peaks=peaks[:3]
+    print ('s_db peaks:', peaks)
+    for peak in peaks:
+        s = str(round(freq[peak]))[:-2]
+        ax2.annotate(s, [freq[peak], s_db[peak]], xytext=[freq[peak]-45, s_db[peak]+5])
+        print (freq[peak], s_db[peak])
+
+#    plt.show()
+    plt.savefig(_filename)
+
+
+
+def sendToFTPServer(_outputdir, _filename):
     import pysftp
     with pysftp.Connection (host='45.76.113.79', username='ftpuser') as sftp:
-       sftp.put (_outputdir + _filename, '/home/ftpuser/ftp/files/' + _filename)
+        print ("Sending %s to FTP server" % _filename)
+        sftp.put (_outputdir + _filename, '/home/ftpuser/ftp/files/' + _filename)
+        print ("FTP transfer complete")
+
+
+
+def dbfft(x, fs, win=None, ref=32768):
+    """
+    Calculate spectrum in dB scale
+    Args:
+        x: input signal
+        fs: sampling frequency
+        win: vector containing window samples (same length as x).
+             If not provided, then rectangular window is used by default.
+        ref: reference value used for dBFS scale. 32768 for int16 and 1 for float
+
+    Returns:
+        freq: frequency vector
+        s_db: spectrum in dB scale
+    """
+
+    N = len(x)  # Length of input sequence
+
+    if win is None:
+        win = np.ones(N)
+    if len(x) != len(win):
+            raise ValueError('Signal and window must be of the same length')
+    x = x * win
+
+    # Calculate real FFT and frequency vector
+    sp = np.fft.rfft(x)
+    freq = np.arange((N / 2) + 1) / (float(N) / fs)
+
+    # Scale the magnitude of FFT by window and factor of 2,
+    # because we are using half of FFT spectrum.
+    s_mag = np.abs(sp) * 2 / np.sum(win)    
+
+    # Convert to dBFS
+    s_dbfs = 20 * np.log10(s_mag / ref)
+
+    return freq, s_mag/max(s_mag), s_dbfs
 
 
 def main():
@@ -126,7 +167,7 @@ def main():
     parser.add_argument("-o", "--outputdir", help="output directory (default is current)")
     parser.add_argument("-r", "--removewav", help="delete the wav file when finished (default is true)", action="store_true")
     parser.add_argument("-f", "--ftp", help="send file to server via FTP (default is false)", action="store_true")
-    parser.add_argument("-l", "--list", help="list available audio devices", action="store_true")
+    parser.add_argument("-ll", "--list", help="list available audio devices", action="store_true")
     
     args = parser.parse_args()
 
@@ -154,27 +195,42 @@ def main():
     
     # grab the audio
     grab_audio(outputdir + WAVE_OUTPUT_FILENAME, audio_index, sample_rate, record_seconds)
-    
-    # perform thh FFT
-    (xf, yf, upper_cutoff) = perform_FFT(outputdir + WAVE_OUTPUT_FILENAME)
-    
+    fs, signal = wav.read(outputdir + WAVE_OUTPUT_FILENAME)
+    #fs, signal = wav.read('Sine_440hz_0dB_10seconds_44.1khz_16bit_mono.wav')
+
+    print ('Data type          :', signal.dtype)
+    print ('Sampling frequency :', fs, 'Hz')
+    print ('Sample length      :', len(signal), 'samples')
+    print ('Audio channels     :', CHANNELS)
+    print ('Audio length       :', '{:0.3f}'.format(len(signal) / fs), 'seconds')
+
+
+    # Take slice
+    N = 8192
+    win = np.hamming(N)
+    freq, s_mag, s_dbfs = dbfft(signal[0:N], fs, win)
+
+    # Scale from dBFS to dB
+    K = 78  # from microphone datasheet
+    s_db = s_dbfs + K
+       
     # create an image and save to disk
     # get the next available image sequence
     i=1
-    while os.path.exists(outputdir + "img%s.png" % i):
+    while os.path.exists(outputdir + socket.gethostname() + "_" + "img%s.png" % i):
         i += 1
-    filename = "img%s.png" % i
+    filename = socket.gethostname() + "_" + "img%s.png" % i
     title = WAVE_OUTPUT_FILENAME.split('.')[0][:-2]     # remove .wav & seconds
-    saveToImage(xf, yf, upper_cutoff, outputdir + filename, title)
+    saveToImage(freq, s_mag, s_db, Y_AXIS_MAX, outputdir + filename, title)
 
     # send the file to the server if needed
     if sendWithFTP:
-        sendWavViaFTP(outputdir, WAVE_OUTPUT_FILENAME)
+        sendToFTPServer(outputdir, WAVE_OUTPUT_FILENAME)
+        sendToFTPServer(outputdir, filename)
 
     # delete the wav if not needed
     if removewav:
-        os.remove(outputdir + WAVE_OUTPUT_FILENAME)
-    
+        os.remove(outputdir + WAVE_OUTPUT_FILENAME)    
     
     
 if __name__ == "__main__":
